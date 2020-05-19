b204db9a977d6d3c536747fc703263a4ad0efd11
Jamie Stivala <jamiestivala@gmail.com>
2020-05-19 20:09:49 +0200

    Removed the need of a config file (pass everything through arguments)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import {exec} from 'child_process'
 import * as mc from 'minecraft-protocol'
-import * as config from '../resources/config.json'
 import {isServerActive, sleep} from './tools'
+
+let args = process.argv.slice(2);
+let host = args[0];
+let port = args[1];
+let systemctlService = args[2];
+let timeout = args[3];
 
 function awaitLogin() : Promise <void> {
     return new Promise<void>(resolve => {
-        let server = mc.createServer({'online-mode': false, host: '0.0.0.0', port: Number(config.port), version: '1.15.2', motd: "Start server by logging in", maxPlayers: 1});
+        let server = mc.createServer({'online-mode': false, host: '0.0.0.0', port: Number(port), version: '1.15.2', motd: "Start server by logging in", maxPlayers: 1});
 
         server.on('login', client => {
             client.write('login', {entityId: client.uuid, levelType: 'default', gameMode: 0, dimension: 0, difficulty: 2, hashedSeed: [0,0], maxPlayers: server.maxPlayers, reducedDebugInfo: false, seed: 1});
@@ -19,20 +24,20 @@ function awaitLogin() : Promise <void> {
 
 async function checkActivity(lastActive : Date, serverStarted : boolean) : Promise<void>{
     await sleep(3000);
-    let active = await isServerActive();
+    let active = await isServerActive(host);
 
     if(active) return checkActivity(new Date(), true);
     else if(!active && !serverStarted) return checkActivity(new Date(), serverStarted);
-    else if(!active && serverStarted && (new Date().getTime()) - lastActive.getTime() < config.timeout) return checkActivity(lastActive, serverStarted);
+    else if(!active && serverStarted && (new Date().getTime()) - lastActive.getTime() < Number(timeout)) return checkActivity(lastActive, serverStarted);
 }
 
 async function serverHandler() : Promise <void> {
     await awaitLogin();
 
-    exec('sudo systemctl start ' + config.systemctlService, () => console.log("SystemCTL start initiated"));
+    exec('sudo systemctl start ' + systemctlService, () => console.log("SystemCTL start initiated"));
     await checkActivity(new Date(), false);
 
-    exec('sudo systemctl stop ' + config.systemctlService, () => console.log("SystemCTL stop initiated"));
+    exec('sudo systemctl stop ' + systemctlService, () => console.log("SystemCTL stop initiated"));
     await sleep(60000);
 
     return serverHandler();
--- src/tools.ts
+++ src/tools.ts
@@ -1,10 +1,9 @@
 import {promisify} from "util";
 import * as mc from "minecraft-protocol";
-import * as config from "../resources/config.json";
 
 const promisePing = promisify(mc.ping)
-export async function isServerActive() : Promise <boolean>{
-    return promisePing({host: config.host}).then((result : mc.NewPingResult) => {return result.players.online > 0;}).catch(() => {return false});
+export async function isServerActive(host : string) : Promise <boolean>{
+    return promisePing({host: host}).then((result : mc.NewPingResult) => {return result.players.online > 0;}).catch(() => {return false});
 }
 
 export function sleep(ms: number) {
