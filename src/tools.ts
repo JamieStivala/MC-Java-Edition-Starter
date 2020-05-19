7f2262ce9537b96a231405c583fbb41844377a34
Jamie Stivala <jamiestivala@gmail.com>
2020-05-19 19:19:09 +0200

    Added a tools function class to store some simple tools

---
+++ src/tools.ts
@@ -0,0 +1,14 @@
+import {promisify} from "util";
+import * as mc from "minecraft-protocol";
+import * as config from "../resources/config.json";
+
+const promisePing = promisify(mc.ping)
+export async function isServerActive() : Promise <boolean>{
+    return promisePing({host: config.host}).then((result : mc.NewPingResult) => {return result.players.online > 0;}).catch(error => {return false});
+}
+
+export function sleep(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
+}
